9afe5d4593f737b6894cb2977c1d1cde7c68532e
YJU <yju0808@naver.com>
2023-10-25 10:33:02 +0900

    update

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -18,6 +18,17 @@ export default class MyDocument extends Document {
             />
 
             <link rel='manifest' href='/manifest.json' />
+
+            <link rel='preconnect' href='https://fonts.googleapis.com' />
+            <link
+              rel='preconnect'
+              href='https://fonts.gstatic.com'
+              crossOrigin='anonymous'
+            />
+            <link
+              href='https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;600;700;800;900&display=swap'
+              rel='stylesheet'
+            ></link>
           </Head>
 
           <body>
