05442beab713c2a0649985afadf300e50cc81439
YJU <yju0808@naver.com>
2023-10-25 09:59:14 +0900

    update

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,14 +1,7 @@
 import * as React from 'react'
 import Document, { Head, Html, Main, NextScript } from 'next/document'
-import { Noto_Sans_KR } from 'next/font/google'
 
 import { IconContext } from '@react-icons/all-files'
-
-const notoSansKr = Noto_Sans_KR({
-  preload: false, // 또는 preload: false
-  weight: ['100', '300', '400', '500', '700', '900'],
-  variable: '--font-noto-sans-kr'
-})
 
 export default class MyDocument extends Document {
   render() {
@@ -27,7 +20,7 @@ export default class MyDocument extends Document {
             <link rel='manifest' href='/manifest.json' />
           </Head>
 
-          <body className={`${notoSansKr.className} ${notoSansKr.variable} `}>
+          <body>
             <script
               dangerouslySetInnerHTML={{
                 __html: `
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,15 @@
 import * as React from 'react'
+import { Noto_Sans_KR } from 'next/font/google'
 
 import { NotionPage } from '@/components/NotionPage'
 import { domain } from '@/lib/config'
 import { resolveNotionPage } from '@/lib/resolve-notion-page'
+
+const notoSansKr = Noto_Sans_KR({
+  preload: false, // 또는 preload: false
+  weight: ['100', '300', '400', '500', '700', '900'],
+  variable: '--font-noto-sans-kr'
+})
 
 export const getStaticProps = async () => {
   try {
@@ -19,5 +26,10 @@ export const getStaticProps = async () => {
 }
 
 export default function NotionDomainPage(props) {
-  return <NotionPage {...props} />
+  return (
+    <NotionPage
+      className={`${notoSansKr.className} ${notoSansKr.variable} `}
+      {...props}
+    />
+  )
 }
